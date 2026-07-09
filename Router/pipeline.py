0c72666d9df4f2e94ee1d76702fff4a1fe630f61
Oskar <grohmanoskar@gmail.com>
2026-07-10 00:21:59 +0200

    Zmieniono funckje run, aby tekst nie ładował się dwa razy, model zmieniono na bielika 1,5 do testowania

---
+++ Router/pipeline.py
@@ -0,0 +1,30 @@
+from sentence_transformers import SentenceTransformer
+import faiss
+from classify import classify_top1
+from rankings import search_hybrid
+from agents import answer
+
+MODEL_NAME = 'sdadas/mmlw-retrieval-roberta-base'
+model = SentenceTransformer(MODEL_NAME)
+
+def run(query:str) -> dict:
+    query_emb = model.encode([query]).astype('float32')
+    faiss.normalize_L2(query_emb)
+
+    agent = classify_top1(query_emb)
+    chunks = search_hybrid(query, agent, k=5)
+
+
+    odpowiedz = answer(query, agent, chunks)
+
+    zrodlo = list(dict.fromkeys(c['url'] for c, score in chunks))
+    return {'agent': agent, "odpowiedz": odpowiedz, 'zrodla': zrodlo}
+
+
+if __name__ == '__main__':
+    wynik = run("nie pamiętam hasła do konta Allegro, jak odzyskać dostęp")
+    print(f"\n[agent: {wynik['agent']}]")
+    print('Źródła:')
+    for url in wynik['zrodla']:
+        print(url)
+ 
