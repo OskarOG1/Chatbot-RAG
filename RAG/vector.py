4fd51fddb605236029eeb2f5d4dba4c142f085b2
Oskar <grohmanoskar@gmail.com>
2026-07-09 14:13:41 +0200

    Added RAG/vector, Index, requirements, set up venv correctly

---
+++ RAG/vector.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+import numpy as np 
+import faiss
+
+def wczytaj_chunki(sciezka: Path) -> tuple[list[dict], np.ndarray]:
+    with open(sciezka, 'r', encoding='utf-8') as r:
+       
+       sciezka_embeddings = Path(__file__).parent / 'embeddings.npy'
+       embeddings = np.load(sciezka_embeddings)
+
+       return json.load(r), embeddings.astype('float32')
+
+def main():
+
+    sciezka_chunks = Path(__file__).parent / 'chunks.json'
+    chunki,embeddings = wczytaj_chunki(sciezka_chunks)
+    nazwy_agentow = ['konto', 'zakupy', 'platnosci']
+
+    for nazwa in nazwy_agentow:
+        indeksy = [i for i, c in enumerate(chunki) if str(c.get('agent', "")).strip().lower() == nazwa]
+        if not indeksy:
+           print(f"Agent [{nazwa}]: Brak pasujących chunków w pliku.")
+           continue
+
+        agenci_chunki = [chunki[i] for i in indeksy]
+        agenci_embeddings = embeddings[indeksy]
+
+        faiss.normalize_L2(agenci_embeddings)
+        wymiar = embeddings.shape[1]
+        index = faiss.IndexFlatIP(768)
+        index.add(agenci_embeddings)
+
+        faiss.write_index(index, str(Path(__file__).parent / f'{nazwa}.faiss'))
+
+        vector_json = Path(__file__).parent /f'chunks_{nazwa}.json'
+        with open(vector_json, 'w', encoding='utf-8') as w:
+         json.dump(agenci_chunki, w, ensure_ascii=False, indent=4)
+       
+        print(f'agent [{nazwa}]:zapisno {len(indeksy)} chunkow i wektorow')
+
+if __name__ == '__main__':
+    main()
